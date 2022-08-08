8560a0ecc385285c3667adeb54268ab3599a6b85
FZ <62817503+Doge-God@users.noreply.github.com>
2022-08-09 01:16:52 +1200

    feat: api intergration & result print

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,22 @@ function App() {
   const IP_API_BASE_URL = "https://ipinfo.io";
 
   const [ipBarInput, setIpBarInput] = useState("");
-  const [ipResult, setIpResult] = useState({});
+  const [ipResult, setIpResult] = useState(
+    {
+      "data": {
+          "ip": "",
+          "city": "",
+          "region": "",
+          "country": "",
+          "loc": "",
+          "org": "",
+          "postal": "",
+          "timezone": ""
+      },
+      "status": 0,
+      "statusText": "No info yet."
+    }
+  );
 
   return (
     <div>
@@ -20,15 +35,31 @@ function App() {
           onChange={fieldContent => (setIpBarInput(fieldContent.target.value))}/><br/>
         <button onClick={searchIp}> START SEARCH </button>
 
-        <p>entered: {ipBarInput}</p>
+        <div>
+          {
+            ipResult === undefined ? 
+            (<p>No info yet.</p>) :
+            (
+              ipResult.status != 200 ? 
+              (<p> ERROR: {ipResult.status} {ipResult.statusText} </p>) :
+              (<ul>
+                <li>IP: {ipResult.data.ip}</li>
+                <li>LOC: {ipResult.data.city} {ipResult.data.region} {ipResult.data.country}</li>
+                <li>POSTAL: {ipResult.data.postal}</li>
+                <li>CORD: {ipResult.data.loc}</li>
+                <li>TZ: {ipResult.data.timezone}</li>
+              </ul>)
+            )
+          }
+        </div>
+
       </div>
 
     </div>
   );
 
   function searchIp() {
-    alert(ipBarInput);
-    axios.get(IP_API_BASE_URL + "/" + ipBarInput + "?token=54370b3dbd9873")
+    axios.get(IP_API_BASE_URL + "/" + ipBarInput + "?token=54370b3dbd9873", {validateStatus: () => true})
       .then((result) => {setIpResult(result)} );
     console.log(ipResult);
   }
