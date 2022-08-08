73f009e16916889f3205220830c95695188b10ab
FZ <62817503+Doge-God@users.noreply.github.com>
2022-08-08 23:27:16 +1200

    feat: basic text framework, chore: delete unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './index.css';
 
 function App() {
+
+  const IP_API_BASE_URL = "https://ipinfo.io/";
+
+  const [ipBarInput, setIpBarInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> *IP INFO SEARCH* </h1>
+      
+      <div>
+        <label>ENTER IP HERE</label><br/>
+        <input type="text" id="ipInput" name="ipInput" 
+          onChange={fieldContent => (setIpBarInput(fieldContent.target.value))}/><br/>
+        <button onClick={searchIp}> START SEARCH </button>
+
+        <p>entered: {ipBarInput}</p>
+      </div>
+
     </div>
   );
+
+  function searchIp() {
+    alert(ipBarInput);
+    console.log(ipBarInput);
+  }
 }
 
 export default App;
